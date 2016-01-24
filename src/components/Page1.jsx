6f90a7f414874b498894a69ffe3a0958c3e17ef1
Greg Chamberlain <gregcdev@gmail.com>
2016-01-23 23:38:19 -0600

    Initial commit

---
+++ src/components/Page1.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var Page1 = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+        <p>Page 1</p>
+      </div>
+    );
+  }
+
+});
+
+module.exports = Page1;
---
+++ src/components/Page2.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var Page2 = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+        <p>Page 2</p>
+      </div>
+    );
+  }
+
+});
+
+module.exports = Page2;
